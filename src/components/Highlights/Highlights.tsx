723c9f70a9b5761ac5d0906a82ee8d1c91eb1613
Ren Binden <ren.binden@gmail.com>
2025-08-14 21:11:49 +0100

    remove unused import

--- src/components/Highlights/Highlights.tsx
+++ src/components/Highlights/Highlights.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import React from "react";
 import Image from "next/image";
 
